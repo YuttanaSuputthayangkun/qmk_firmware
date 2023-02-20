2ed16a6b4f3e8eec37849ffb3f8755650434e409
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2023-02-20 18:22:16 +0700

    use layer tap in Ciel

--- keyboards/chickenman/ciel/keymaps/Jugemu1/keymap.c
+++ keyboards/chickenman/ciel/keymaps/Jugemu1/keymap.c
@@ -19,20 +19,39 @@
 
 enum layer_names {
     _BASE = 0,
-    _FN = 1,
-    _FN1 = ARROW_LAYER,
+    _FN,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
 };
 
-#define F_MM LT(CK_MM,KC_F)
-#define J_MM LT(CK_MM,KC_J)
+// lower mod
+#define LM_TAB LT(_LOWER, KC_TAB)
+#define LM_Z LT(_LOWER, KC_Z)
+
+// raise mod
+#define LM_QUOT LT(_RAISE, KC_QUOT)
+#define LM_SLSH LT(_RAISE, KC_SLSH)
+
+// raise mod activated
+#define LM_GRV LT(_ADJUST, KC_GRV)
+#define LM_F8 LT(_ADJUST, KC_F8)
+
+// lower mod activated
+#define LM_TILD LT(_ADJUST, KC_TILD)
+// #define LM_UP LT(_ADJUST, KC_UP)
+#define LM_RCBR LT(_ADJUST, KC_RCBR)
+
+#define MO_LOW MO(_LOWER)
+#define MO_RIS MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_60_ansi_split_bs_rshift(
         KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,     KC_EQL,     KC_GRV,    KC_BSPC,
-        KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,                KC_BSLS,
-        KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, ARROW_SCLN,    KC_QUOT,                             KC_ENT,
-        KC_LSFT,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,  KC_SLSH,               KC_UP,   MO(_FN),
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                           _______,   KC_LEFT,  KC_DOWN,   KC_RIGHT
+        LM_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,                KC_BSLS,
+        KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LM_QUOT,                             KC_ENT,
+        KC_LSFT,    LM_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  LM_SLSH,                           KC_UP,    MO(_FN),
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                           _______,   KC_LEFT,   KC_DOWN,   KC_RIGHT
     ),
     [_FN] = LAYOUT_60_ansi_split_bs_rshift(
         QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,KC_VOLD, KC_VOLU,
@@ -41,12 +60,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       CK_TD_MODE, _______,
         _______, _______, _______,                            _______,                                     _______, _______, _______, _______
     ),
-    [_FN1] = LAYOUT_60_ansi_split_bs_rshift(
-        KC_GRAVE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,     KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,  KC_DEL,
-        _______, _______, _______, _______, _______,  CK_SWL, _______,    KC_HOME,    KC_END, _______,  _______, _______, _______,           _______,
-        _______, _______, _______, _______, _______, _______, KC_LEFT,    KC_DOWN,     KC_UP,KC_RIGHT,  _______, _______,                    _______,
-        _______,          _______, _______, _______, KC_BSPC, _______,    _______,   KC_PGUP, KC_PGDN,   _______, _______,        CK_TD_MODE, _______,
-        _______, _______, _______,                            KC_PENT,                                             _______, _______, _______, _______
+
+    // [_FN] = LAYOUT_60_ansi_split_bs_rshift(
+    //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+    //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______,
+    //     _______, _______, _______,                            _______,                                     _______, _______, _______, _______
+    // ),
+
+
+    [_LOWER] = LAYOUT_60_ansi_split_bs_rshift(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,          _______,
+TD(TD_LCTL_GRV), KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, LM_TILD,                   _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, LM_RCBR,                   _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______, _______
+    ),
+
+    // [_RAISE] = LAYOUT(
+    // _______,         _______, _______, _______, _______, _______,                       KC_VOLD, KC_VOLU,TD_AUDIO,CK_PPSCR,  CK_FPSCR,  KC_DEL,
+    // KC_GRV,             KC_1,     KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,      KC_0, KC_VOLU,
+    // TD(TD_RCTL_F1),    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                      KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, _______, KC_VOLD,
+    // TD(TD_LSFT_F7),    KC_F8,    KC_F9,  KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS,  KC_EQL, KC_LBRC,   KC_RBRC, _______,
+    //                                     _______, _______, _______,   _______, _______,  _______, _______, _______
+    // ),
+
+    [_RAISE] = LAYOUT_60_ansi_split_bs_rshift(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+         LM_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, _______,          KC_VOLU,
+ TD(TD_RCTL_F1),   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,                   KC_VOLD,
+ TD(TD_LSFT_F7),   LM_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_PLUS, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC,                   _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______, _______
+    ),
+
+    // [_ADJUST] = LAYOUT(
+    // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, T_EROGE,T_READING,                   T_BASE, T_GENSH,  T_GAME, XXXXXXX, XXXXXXX, XXXXXXX,
+    // XXXXXXX, XXXXXXX,  KC_INS, KC_HOME, KC_PGUP, XXXXXXX,                   XXXXXXX, KC_KP_7, KC_KP_8, KC_KP_9, XXXXXXX, XXXXXXX,
+    // XXXXXXX, XXXXXXX,  KC_DEL,  KC_END, KC_PGDN, XXXXXXX,                    KC_DOT, KC_KP_4, KC_KP_5, KC_KP_6, XXXXXXX, XXXXXXX,
+    // XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, XXXXXXX, XXXXXXX,
+    //                             _______, _______, _______, _______, _______,  _______, _______, _______
+    // ),
+
+    [_ADJUST] = LAYOUT_60_ansi_split_bs_rshift(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______,  KC_INS, KC_HOME, KC_PGUP, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______, _______,          _______,
+        _______, _______,  KC_DEL,  KC_END, KC_PGDN, _______,  KC_DOT, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______,                   _______,
+        _______, _______, _______, _______, _______, _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, _______,                   _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______, _______
     ),
 };
 
