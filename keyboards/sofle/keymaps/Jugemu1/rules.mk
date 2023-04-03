4d287b7d9f78a26e3687a65cb09b31cc08511a97
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2023-04-03 15:03:57 +0700

    add bootmagic and mouse key

--- keyboards/sofle/keymaps/Jugemu1/rules.mk
+++ keyboards/sofle/keymaps/Jugemu1/rules.mk
@@ -1,13 +1,13 @@
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
 CONSOLE_ENABLE = no
 EXTRAKEY_ENABLE = yes
+RGBLIGHT_ENABLE = no
 
 SWAP_HANDS_ENABLE = no
 
@@ -15,7 +15,7 @@ SWAP_HANDS_ENABLE = no
 # https://github.com/qmk/qmk_firmware/blob/master/docs/feature_converters.md
 # https://github.com/qmk/qmk_firmware/issues/18746
 # consider using #ifdef CONVERT_TO_PROMICRO_RP2040 for condition
-# CONVERT_TO=promicro_rp2040
+CONVERT_TO=promicro_rp2040
 
 ifeq ($(strip $(OLED_ENABLE)), yes)
 	SRC += ./timelogger.c
@@ -23,8 +23,7 @@ endif
 
 ifeq ($(strip $(CONVERT_TO)), promicro_rp2040)
 	ENCODER_ENABLE = yes
-	RGBLIGHT_ENABLE = yes
-else
-	ENCODER_ENABLE = no
-	RGBLIGHT_ENABLE = no
+	NKRO_ENABLE = yes
+	MOUSEKEY_ENABLE = yes        # Mouse keys
+	BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
 endif
