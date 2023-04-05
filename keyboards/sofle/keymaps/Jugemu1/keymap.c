a0f5475f818340ae4cd87e94252c6f5c5f53df16
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2023-04-05 14:06:17 +0700

    change right side button to left mouse click

--- keyboards/sofle/keymaps/Jugemu1/keymap.c
+++ keyboards/sofle/keymaps/Jugemu1/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
     KC_LCTL,       KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,    KC_QUOT,
     KC_LSFT,       KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, CK_TD_MODE, TG(_GAME_GENSHIN), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PIPE,
-                      KC_LALT, KC_BTN1, KC_LGUI, MO(_LOWER), KC_SPC,          KC_ENT, MO(_RAISE), KC_BSPC, TG(_GAME_GENSHIN), KC_RGUI
+                      KC_LALT, KC_BTN1, KC_LGUI, MO(_LOWER), KC_SPC,          KC_ENT, MO(_RAISE), KC_BSPC, TG(_GAME_GENSHIN), KC_BTN1
     ),
     [_LOWER] = LAYOUT(
     _______,          KC_WBAK, KC_WFWD,  KC_WH_L, KC_WH_R, _______,                   _______, _______, _______,_______, _______, _______,
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,    KC_1,   KC_2,    KC_3,    KC_4,    KC_M,                                  KC_VOLD,    KC_VOLU,TD_AUDIO,    KC_9,   CK_FPSCR,   TD_AUDIO,
     KC_TAB,    KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                  KC_Y,    KC_U,    KC_I,    KC_O,       KC_P,    KC_VOLU,
     KC_LALT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_J,                                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_VOLD,
-    KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, T_GENSH,                T_GENSH, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,
+    KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, T_GENSH,                T_GENSH, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_BTN2,    KC_BTN1,
                     _______, _______, KC_LCTL, KC_SPC, MO(_LOWER),         KC_ENT, MO(_RAISE), KC_BSPC, TG(_GAME_GENSHIN), KC_RGUI
     ),
 
