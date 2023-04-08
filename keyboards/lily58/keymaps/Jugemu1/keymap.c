4ff841f0cd851e1d606fb3a153aafe69af64972d
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2023-04-08 23:59:09 +0700

    update lily58 layout

--- keyboards/lily58/keymaps/Jugemu1/keymap.c
+++ keyboards/lily58/keymaps/Jugemu1/keymap.c
@@ -150,8 +150,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 
     [_GAME] = LAYOUT(
-    KC_ESC,        KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                _______, _______,  _______,   _______,    KC_0,    KC_GRV,
-    KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                   KC_Y,    KC_U,    KC_UP,      KC_O,    KC_P,    KC_MINS,
+    KC_ESC,        KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                _______, _______,  _______,   _______,CK_FPSCR, KC_GRV,
+    KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                   KC_Y,    KC_U,    KC_UP,      KC_O,    KC_P, KC_MINS,
     KC_LCTL,       KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                   KC_H, KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_SCLN, KC_QUOT,
     KC_LSFT,       KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  CK_TD_MODE,        T_GAME,       KC_N,    KC_M,  KC_COMM,    KC_DOT, KC_SLSH, KC_RSFT,
                               KC_LALT,   KC_LGUI,MO(_LOWER), KC_SPC,            KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
@@ -166,10 +166,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_EROGE] = LAYOUT(
-    _______,    _______,  _______,   KC_VOLD,    KC_VOLU,      KC_MUTE,                                   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,    _______,  _______,   KC_VOLD,    KC_VOLU,      KC_MUTE,                                   _______,    _______,    _______,    _______,   CK_FPSCR,    _______,
     _______,    _______,  _______,   _______,    _______,      _______,                                   _______,    _______,    _______,    _______,    _______,    _______,
     _______,    _______,  _______,   _______,    _______,      _______,                                   _______,    _______,    _______,    _______,    _______,    _______,
-    _______,    _______,  _______,   _______,    _______,      _______,    T_EROGE,        T_EROGE,       _______,    _______,    _______,    _______,    _______,    _______,
+    _______,    _______,  _______,   _______,    _______,      _______,   CK_FPSCR ,        T_EROGE,      _______,    _______,    _______,    _______,    _______,    _______,
                                     _______, _______,  MO(_GAME_LOWER),     KC_ENT,         _______, _______, _______, _______
     ),
 
