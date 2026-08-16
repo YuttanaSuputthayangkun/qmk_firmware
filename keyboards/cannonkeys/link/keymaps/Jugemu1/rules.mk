35001e0f5713825dae99611f6467ab937f5780b3
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2026-08-16 22:39:06 +0700

    add: old oled code, lib from lily58

--- keyboards/cannonkeys/link/keymaps/Jugemu1/rules.mk
+++ keyboards/cannonkeys/link/keymaps/Jugemu1/rules.mk
@@ -2,3 +2,8 @@ TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
 OLED_ENABLE = yes
 ENCODER_ENABLE = yes
+
+ifeq ($(strip $(OLED_ENABLE)), yes)
+	SRC += ./lib/keylogger.c
+	SRC += ./lib/timelogger.c
+endif
