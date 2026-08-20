7dcd8cbcbd25842f37894065fc1eac969d05cb17
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2026-08-20 16:56:49 +0700

    update: keymap: add boot to rotary in lower layer

--- keyboards/cannonkeys/link/keymaps/Jugemu1/keymap.c
+++ keyboards/cannonkeys/link/keymaps/Jugemu1/keymap.c
@@ -714,7 +714,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,       KC_VOLD,      KC_VOLU,      TD_AUDIO,       _______,        _______,                                  _______,       _______,      _______,        _______,       _______,       _______,
     KC_F1,         KC_F2,        KC_F3,         KC_F4,         KC_F5,          KC_F6,                                    KC_F7,         KC_F8,        KC_F9,         KC_F10,        KC_F11,        KC_F12,
 TD(TD_LCTL_GRV), KC_EXLM,        KC_AT,       KC_HASH,        KC_DLR,        KC_PERC,                                  KC_CIRC,       KC_AMPR,      KC_ASTR,        KC_LPRN,       KC_RPRN,       KC_TILD,
-  _______,       _______,      _______,       _______,       _______,        _______,      _______,     _______,       XXXXXXX,       KC_UNDS,      KC_PLUS,        KC_LCBR,       KC_RCBR,       KC_PIPE,
+  _______,       _______,      _______,       _______,       _______,        _______,      QK_BOOT,     _______,       XXXXXXX,       KC_UNDS,      KC_PLUS,        KC_LCBR,       KC_RCBR,       KC_PIPE,
                  _______,      _______,       _______,       _______,        _______,                                  _______,       _______,      _______,        MS_WHLU,       MS_WHLD
 ),
 
